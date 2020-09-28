9436c294431fdf76101158531c9dc05ca14c0597
rasmusmerzin <rasmusmerzin@gmail.com>
2020-09-28 23:12:31 +0300

    users route & database controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
+import users from "./users";
+router.use("/users", users);
+
 router.get("/", (_, res) => res.end("Hello, World!"));
 
 export default router;
